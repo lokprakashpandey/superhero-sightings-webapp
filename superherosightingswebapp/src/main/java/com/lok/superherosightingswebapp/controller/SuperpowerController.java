832cad6370ffd4284d17c0d3d17595fcd9e361ef
Lok Prakash Pandey <lokprakashpandey@gmail.com>
2023-03-28 09:32:48 -0400

    Superpower controller and view added

--- superherosightingswebapp/src/main/java/com/lok/superherosightingswebapp/controller/SuperpowerController.java
+++ superherosightingswebapp/src/main/java/com/lok/superherosightingswebapp/controller/SuperpowerController.java
@@ -14,10 +14,15 @@ import com.lok.superherosightingswebapp.dao.SuperheroDao;
 import com.lok.superherosightingswebapp.dao.SuperpowerDao;
 import com.lok.superherosightingswebapp.dto.Superpower;
 import java.util.List;
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class SuperpowerController {
@@ -38,9 +43,56 @@ public class SuperpowerController {
     SuperpowerDao superpowerDao;
 
     @GetMapping("superpowers")
-    public String displayTeachers(Model model) {
+    public String displaySuperpowers(Model model) {
         List<Superpower> superpowers = superpowerDao.getAllSuperpowers();
-        model.addAttribute("teachers", superpowers);
+        model.addAttribute("superpowers", superpowers);
         return "superpowers";
+    }
+    
+    @PostMapping("addSuperpower")
+    public String addSuperpower(@Valid Superpower superpower, BindingResult result, Model model) {
+        if(result.hasErrors()) {
+            //Set model to retrive data entered
+            model.addAttribute("superpower",superpower);
+            //Catch error for display
+            FieldError nameError = result.getFieldError("name");
+            model.addAttribute("nameError", nameError);
+            FieldError descriptionError = result.getFieldError("description");
+            model.addAttribute("descriptionError", descriptionError);
+            //Set superpowers list also to display, otherwise nothing will be displayed in the table
+            List<Superpower> superpowers = superpowerDao.getAllSuperpowers();
+            model.addAttribute("superpowers", superpowers);//This is superpowers list
+            return "superpowers"; // This is view
+        }
+        else {
+            superpowerDao.addSuperpower(superpower);
+            return "redirect:/superpowers";
+        }
+    }
+    
+    @GetMapping("editSuperpower")
+    public String editTeacher(HttpServletRequest request, Model model) {
+        int id = Integer.parseInt(request.getParameter("id"));
+        Superpower superpower = superpowerDao.getSuperpowerById(id);
+        model.addAttribute("superpower", superpower);
+        return "editSuperpower";
+    }
+    
+    @PostMapping("editSuperpower")
+    public String performEditCourse(@Valid Superpower superpower, BindingResult result, Model model) {
+                
+        if(result.hasErrors()) {
+            model.addAttribute("superpower", superpower);
+            return "editSuperpower";
+        }
+        superpowerDao.updateSuperpower(superpower);
+        return "redirect:/superpowers";
+    }
+    
+    @GetMapping("deleteSuperpower")
+    public String deleteSuperpower(HttpServletRequest request) {
+        int id = Integer.parseInt(request.getParameter("id"));
+        superpowerDao.deleteSuperpowerById(id);
+        return "redirect:/superpowers";
     }
 }
--- superherosightingswebapp/src/main/java/com/lok/superherosightingswebapp/dto/Superpower.java
+++ superherosightingswebapp/src/main/java/com/lok/superherosightingswebapp/dto/Superpower.java
@@ -7,11 +7,19 @@
 package com.lok.superherosightingswebapp.dto;
 
 import java.util.Objects;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 public class Superpower {
 
     private int id;
+    
+    @NotBlank(message = "Name must not be empty.")
+    @Size(max = 50, message = "Name must be less than 50 characters.")
     private String name;
+    
+    @NotBlank(message = "Description must not be empty.")
+    @Size(max = 255, message = "Description must be less than 255 characters.")
     private String description;
 
     public int getId() {
